5993bbe0dc49dc1aa22ecd178d351dc78cbbc175
sosrtv353 <sosortveliashvili@gmail.com>
2026-01-20 21:41:18 +0400

    fix paths and folder names

--- web game/game/intro.js
+++
@@ -1,2 +0,0 @@
-let button = document.querySelector('button');
-button.addEventListener('click', () => {window.location.href = "main.html";});
---
+++ web-game/game/intro.js
@@ -0,0 +1,2 @@
+let button = document.querySelector('button');
+button.addEventListener('click', () => {window.location.href = "./web-game/game/main.html";});
